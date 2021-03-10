14429d057ad35999ed69178a3b43cd816d327031
Mridul Khare <mridulkhr4@gmail.com>
2021-03-10 23:32:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/cloneapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.android.cloneapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+
+        toolbar.setTitle(R.string.app_name);
+        setSupportActionBar(toolbar);
+    }
+}
